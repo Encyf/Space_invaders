3132b6608b7d7984fc1bcb9e0dfec8361e833383
Encyf <67859752+Encyf@users.noreply.github.com>
2020-07-14 07:26:36 +0200

    Update ship.py

--- venv/lib/ship.py
+++ venv/lib/ship.py
@@ -9,13 +9,16 @@ class Ship:
         # TODO: Trzeba skalować
         self.rect = self.image.get_rect()
         self.rect.midleft = self.screen_rect.midleft
+        self.y = float(self.rect.y)
         self.moving_up = False
         self.moving_down = False
     def update(self):
-        if self.moving_up:
-            self.rect.y -= self.settings.ship_speed
-        if self.moving_down:
-            self.rect.y += self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+
+        self.rect.y = self.y
 
 
     def blitme(self):
