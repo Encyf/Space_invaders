f68f5ad19038977e0e0c74d6189fa0bdb3133988
Encyf <67859752+Encyf@users.noreply.github.com>
2020-07-14 07:55:37 +0200

    Update ship.py

--- venv/lib/ship.py
+++ venv/lib/ship.py
@@ -6,12 +6,13 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
         self.image = pygame.image.load('/Users/RBa/PycharmProjects/Space_invaders/venv/images/FishEgg-icon.png')
-        # TODO: Trzeba skalować
+        # TODO: Trzeba skalować ten obrazek, musze zobaczyć jak to zrobić, ponieważ jest za duzy.
         self.rect = self.image.get_rect()
         self.rect.midleft = self.screen_rect.midleft
         self.y = float(self.rect.y)
         self.moving_up = False
         self.moving_down = False
+
     def update(self):
         if self.moving_up and self.rect.top > 0:
             self.y -= self.settings.ship_speed
@@ -19,7 +20,6 @@ class Ship:
             self.y += self.settings.ship_speed
 
         self.rect.y = self.y
-
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
