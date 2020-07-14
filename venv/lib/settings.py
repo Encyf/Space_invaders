a9facf6599c2f2d26b16a0d0ee776067e609d326
Encyf <67859752+Encyf@users.noreply.github.com>
2020-07-14 15:29:51 +0200

    Update settings.py

--- venv/lib/settings.py
+++ venv/lib/settings.py
@@ -1,7 +1,13 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 600
-        self.screen_height = 600
+        self.screen_width = 1200
+        self.screen_height = 800
         #ciekawe czy uda się zdefiniowac taki event żeby obraz rosnął po naciśnięciu konkretnego klawisza?
         self.bg_color = (50, 50, 125)
-        self.ship_speed = 1.5
+        self.ship_speed = 20
+        self.Full_screen = 0
+
+        self.bullet_speed = 1.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (255, 0, 0)
