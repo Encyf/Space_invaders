239a55672b6b5f856db3ec4b3d0e6e8d9ef5ca91
Encyf <67859752+Encyf@users.noreply.github.com>
2020-07-19 12:57:40 +0200

    Update alien.py

--- venv/lib/alien.py
+++ venv/lib/alien.py
@@ -5,13 +5,17 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load('/Users/RBa/PycharmProjects/Space_invaders/venv/images/FishEgg-icon.png')
+
+
+        self.image = pygame.image.load('/Users/RBa/PycharmProjects/Space_invaders/venv/images/Ball_MS.png')
         self.rect = self.image.get_rect()
 
-        self.rect.x = self.rect.right
-        self.rect.y = self.rect.top
+        self.rect.midright = self.screen_rect.midright
+        #self.rect.x = self.rect.right
+        #self.rect.y = self.rect.top
+
 
         #self.x = float(self.rect.x)
 
-print(Alien.__dict__)
