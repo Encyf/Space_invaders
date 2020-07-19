03efa76acfaef9421913ced3c40d914aae8b75cf
Encyf <67859752+Encyf@users.noreply.github.com>
2020-07-19 12:57:26 +0200

    Update settings.py

--- venv/lib/settings.py
+++ venv/lib/settings.py
@@ -9,7 +9,7 @@ class Settings:
         self.Full_screen = 0
 
         self.bullet_speed = 2.0
-        self.bullet_width = 9
-        self.bullet_height = 3
+        self.bullet_width = 6
+        self.bullet_height = 6
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
